90aa0c5392b0ab8089675989c1f71b2c6f35e102
aaron blum <blumaa@gmail.com>
2019-10-09 15:59:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function submitData(name, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  }
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(object => {
+      document.body.innerHTML = object['id'];
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message;
+    });
+}
